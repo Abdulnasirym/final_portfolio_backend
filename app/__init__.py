7795cbb6e6170db6d74d88ccbc54fbe1c816c08f
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2025-01-19 23:25:31 +0100

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_migrate import Migrate
+from flask_mail import Mail
+from config import Config
 
 # Initialize extensions
 db = SQLAlchemy()
+migrate = Migrate()
+mail = Mail()
 
 def create_app():
 	app = Flask(__name__)
@@ -14,7 +18,15 @@ def create_app():
 
 	# Initialize the extensions
 	db.init_app(app)
+	migrate.init_app(app, db)
+	mail.init_app(app)
 
 	# import and register blueprints
+	
+	from .routes.supplements_routes import supplements
+	from .routes.notification_routes import notifications
+
+	app.register_blueprint(supplements)
+	app.register_blueprint(notifications)
 
 	return app	
---
+++ app/models/supplements.py
@@ -0,0 +1,12 @@
+from app import db
+from datetime import datetime
+import uuid
+
+class Supplement(db.Model):
+	__tablename__ = 'supplements'
+
+	id = db.Column(db.String(10), primary_key=True, default=lambda: str(uuid.uuid4()))
+	name = db.Column(db.String(100), nullable=False)
+	description = db.Column(db.Text, nullable=True)
+	created_at = db.Column(db.DateTime, default=datetime.utcnow)
+	updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
